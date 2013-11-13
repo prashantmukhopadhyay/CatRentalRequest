744d87355fb8b535cd14b6769aeeda069c04dcae
coleman+prashant <coleman_prashant@appacademy.io>
2013-11-13 09:20:02 -0500

    create cats table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatRental::Application.config.secret_token = '1a55f9640d931a8a480ae987c076ca317e220b97887622fa598d5d41f0e3531d72057ac71cb83acc32a2c97c7a2604d446b3ee26c67775cdb5e80f66f05f24f8'
